81dc0e310e68e8dfcac778997fff8b26ed4b9957
Sona1705 <sonamaha0517@gmail.com>
2025-03-11 15:34:29 +0530

    Added joystick teleoperation config and setup files

---
+++ src/amr_controller/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+package_name = 'amr_controller'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='npd',
+    maintainer_email='npd@example.com',
+    description='AMR Controller Package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'simple_controller = amr_controller.simple_controller:main',
+        ],
+    },
+)
