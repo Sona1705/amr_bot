37aafe06b5108ba5e8f3e7f82cb604af315a50a6
Sona1705 <sonamaha0517@gmail.com>
2025-03-11 15:37:14 +0530

    Updated AMR controller, description, and examples

--- src/amr_controller/launch/controller.launch.py
+++ src/amr_controller/launch/controller.launch.py
@@ -1,47 +1,66 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import TimerAction, DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, TimerAction, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, Command 
+from launch_ros.actions import Node 
+from launch.conditions import IfCondition
+import xacro
 
+def launch_setup(context, *args, **kwargs):
 
-def generate_launch_description():
+    use_python = LaunchConfiguration("use_python").perform(context)
+    wheel_radius = float(LaunchConfiguration("wheel_radius").perform(context))
+    wheel_separation = float(LaunchConfiguration("wheel_separation").perform(context))
+
     
-    controllers_yaml = os.path.join(
-        get_package_share_directory('amr_controller'), 
-        'config',
-        'amr_controllers.yaml'
+    use_python_arg = DeclareLaunchArgument(
+        "use_python",
+        default_value="False",
+        description="Use Python-based simple controller if set to True"
+    )
+    
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius",
+        default_value="0.08"
+    )
+    
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation",
+        default_value="0.594"
     )
 
-    # Get the robot description URDF path
+    controller_yaml = os.path.join(
+        get_package_share_directory("amr_controller"),
+        "config",
+        "amr_controllers.yaml"
+    )
+
     robot_description = os.path.join(
-        get_package_share_directory('amr_description'),
-        'urdf',
-        'amr_ros2_control.xacro'
+        get_package_share_directory("amr_description"),
+        "urdf",
+        "amr_ros2_control.xacro"
     )
 
-    # Start the robot description node
     robot_description_node = Node(
-        package='xacro',
-        executable='xacro',
-        name='robot_description',
-        output='screen',
-        arguments=['--inorder', robot_description]
+        package="xacro",
+        executable="xacro",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[{"--inorder", robot_description}]
     )
+    
 
-    # Start controller manager
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[controllers_yaml],
+        parameters=[controller_yaml],
         output="screen"
     )
 
-    # Spawner for joint state broadcaster
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
-        executable="spawner",  # Make sure to use spawner.py
+        executable="spawner", 
         arguments=[
             "joint_state_broadcaster",
             "--controller-manager",
@@ -49,11 +68,10 @@ def generate_launch_description():
         ],
         output="screen"
     )
-
-    # Spawner for simple velocity controller
+    
     simple_controller_spawner = Node(
         package="controller_manager",
-        executable="spawner",  # Make sure to use spawner.py
+        executable="spawner", 
         arguments=[
             "simple_velocity_controller",
             "--controller-manager",
@@ -64,19 +82,47 @@ def generate_launch_description():
 
     # Timer to delay launching joint state broadcaster
     joint_state_timer = TimerAction(
-        period=3.0,  # Delay by 3 seconds
+        period=3.0, # Delay by 3 seconds
         actions=[joint_state_broadcaster_spawner]
     )
 
     # Timer to delay launching velocity controller
     simple_controller_timer = TimerAction(
-        period=5.0,  # Delay by 5 seconds
+        period=5.0, # Delay by 5 seconds
         actions=[simple_controller_spawner]
     )
 
-    return LaunchDescription([
-        robot_description_node,  # Load the URDF description first
-        controller_manager,      # Start controller manager
-        joint_state_timer,       # Delay joint_state_broadcaster spawning
-        simple_controller_timer  # Delay simple_velocity_controller spawning
+    simple_controller_py = Node(
+        package="amr_controller",
+        executable="simple_controller.py",
+        parameters=[{"wheel_radius": wheel_radius,
+                     "wheel_separation": wheel_separation
+        }],
+        output="screen",
+        
+        condition=IfCondition(use_python)
+        
+    )
+
+    return ([
+        use_python_arg,
+        wheel_radius_arg,
+        wheel_separation_arg,
+        robot_description_node,
+        controller_manager,
+        simple_controller_py,
+        joint_state_timer, 
+        simple_controller_timer 
     ])
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument("use_python", default_value="False", description="Use Python-based simple controller if set to True"),
+        DeclareLaunchArgument("wheel_radius", default_value="0.08"),
+        DeclareLaunchArgument("wheel_separation", default_value="0.594"),
+        OpaqueFunction(function=launch_setup)
+    ])
+    
+    
+
+    
